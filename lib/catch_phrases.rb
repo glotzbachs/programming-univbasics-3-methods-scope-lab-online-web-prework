a0303da368fb68cff1487b9fe59494d73ab6a93c
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-09-17 04:07:10 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -5,5 +5,13 @@ def mario
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+def any_phrase
+  
